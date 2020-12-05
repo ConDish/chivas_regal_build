71db639b90a6d4a6c696ce9a966e18983b0559a1
ConDish <samioff15@gmail.com>
2020-12-05 18:52:41 -0500

    fix

---
+++ precache-manifest.fa4eac410f990126b03e1cb6290c54a8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11e7a0c9a68d1dd74e3a2f009aaf202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d51581cb7677e499e7a8",
+    "url": "/static/css/2.47706c66.chunk.css"
+  },
+  {
+    "revision": "cdfa838125a3080ea587",
+    "url": "/static/css/main.52c5b605.chunk.css"
+  },
+  {
+    "revision": "d51581cb7677e499e7a8",
+    "url": "/static/js/2.84b0b5c5.chunk.js"
+  },
+  {
+    "revision": "621e4a5db71873e1b26a1dbee52e9d1e",
+    "url": "/static/js/2.84b0b5c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdfa838125a3080ea587",
+    "url": "/static/js/main.89c77229.chunk.js"
+  },
+  {
+    "revision": "ac35fe890379ac4d8e72",
+    "url": "/static/js/runtime-main.e6af507c.js"
+  },
+  {
+    "revision": "df6d449d530cf1242f81eabee6f2f452",
+    "url": "/static/media/Inicio.df6d449d.jpg"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "2e0d8cb0c4783406662ca04fec41dbdc",
+    "url": "/static/media/bg-general.2e0d8cb0.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "d675d2dc9a58db99a2b0fecbaa88a324",
+    "url": "/static/media/logo-Juntos.d675d2dc.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2de18b3f04e5f079f1b4a5738dd4d36c",
+    "url": "/static/media/logos-base.2de18b3f.png"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "51fa5df6d4559b29ff175b91a30fbda9",
+    "url": "/static/media/numeral-juntos-otravez.51fa5df6.png"
+  }
+]);
