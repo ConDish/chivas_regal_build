eb6ee390df42ebcca91cbd1840182f05b8c0dcea
ConDish <samioff15@gmail.com>
2020-12-05 18:29:09 -0500

    [Add]: add finish

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 
-app.set("port", process.env.PORT || 4000);
+app.set("port", process.env.PORT || 3000);
 app.use(express.static(`${__dirname}/`));
 
 app.get("*", (req, res) => {
